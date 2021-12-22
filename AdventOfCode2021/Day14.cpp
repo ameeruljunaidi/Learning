5b7a86866af19a80c47171a5f5dd308a78829381
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-12-22 11:09:21 -0500

    Attempted day 14, doesn't work for part b

---
+++ AdventOfCode2021/Day14.cpp
@@ -0,0 +1,112 @@
+#include "Utilities.cpp"
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution
+{
+  private:
+    static std::vector<std::string> input;
+    static std::string polymer;
+    static std::unordered_map<std::string, std::string> pairs;
+    static std::unordered_map<char, int> charCount;
+    static int MostLeastElementDifference();
+
+  public:
+    Solution();
+    static void Solve();
+};
+
+std::vector<std::string> Solution::input;
+std::string Solution::polymer;
+std::unordered_map<std::string, std::string> Solution::pairs;
+std::unordered_map<char, int> Solution::charCount;
+
+Solution::Solution()
+{
+    input = Utilities::ReadLines("AdventOfCode2021/Day14.txt");
+
+    int lineCounter = 0;
+    for (const std::string &line : input)
+    {
+        if (lineCounter == 0)
+        {
+            polymer = line;
+            lineCounter++;
+            continue;
+        }
+
+        std::vector<std::string> splitLine = Utilities::Split(line, " -> ");
+        pairs[splitLine[0]] = splitLine[1];
+    }
+}
+
+int Solution::MostLeastElementDifference()
+{
+    // Function returns the count of the majority element and the minority element
+    // Go through the string n number of times
+    // Create a copy of the polymer first
+    // Need to access the index of the string and loop through
+    // Find if the combination of letters exists in the pairs
+    // Insert into the current position
+
+    int steps = 10;
+    for (int x = 0; x < steps; x++)
+    {
+        std::string copyPolymer(polymer);
+
+        int insertIndex = 0; //  Keeps track of the index to insert the character
+        for (int i = 1; i < polymer.size(); i++)
+        {
+            std::string substring = polymer.substr(i - 1, 2);
+            copyPolymer.insert(i + insertIndex, pairs[substring]);
+            insertIndex++;
+        }
+
+        polymer.swap(copyPolymer);
+    }
+
+    // Initialize the count map
+    for (char i = 'A'; i <= 'Z'; i++)
+    {
+        charCount[i] = 0;
+    }
+
+    for (const char &character : polymer)
+    {
+        charCount[character]++;
+    }
+
+    int minValue = static_cast<int>(polymer.size());
+    int maxValue = 0;
+    for (const auto &[key, value] : charCount)
+    {
+        if (value == 0)
+        {
+            continue;
+        }
+        if (value < minValue)
+        {
+            minValue = value;
+        }
+        if (value > maxValue)
+        {
+            maxValue = value;
+        }
+    }
+
+    return maxValue - minValue;
+}
+
+void Solution::Solve()
+{
+    std::cout << "Solution: " << MostLeastElementDifference() << '\n';
+}
+
+int main()
+{
+    Solution solution;
+    Solution::Solve();
+
+    return 0;
+}
--- AdventOfCode2021/Utilities.cpp
+++ AdventOfCode2021/Utilities.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
+#include <filesystem>
 #include <fstream>
 #include <string>
-#include <filesystem>
 #include <vector>
 
 static const int bits = 12;
@@ -9,26 +9,29 @@ static const int bits = 12;
 class Utilities
 {
   public:
-    static std::vector<std::string> ReadLines(const std::filesystem::path &Path);
-    static int GetFirstLineLength(const std::filesystem::path &Path);
+    static std::vector<std::string> ReadLines(const std::__fs::filesystem::path &Path);
+    static int GetFirstLineLength(const std::__fs::filesystem::path &Path);
     static std::vector<std::bitset<bits>> ConvertToBitset(const std::vector<std::string> &Strings);
     static std::vector<std::string> Split(const std::string &s, const std::string &delimiter);
     static std::vector<int> ConvertToInt(const std::vector<std::string> &Vec);
     static void VerifyElseCrash(bool bAssert);
     static void VerifyNotReached();
-    static std::vector<int> GetBoardRow(const std::string& basicString);
+    static std::vector<int> GetBoardRow(const std::string &basicString);
 };
 
-std::vector<std::string> Utilities::ReadLines(const std::filesystem::path &Path)
+std::vector<std::string> Utilities::ReadLines(const std::__fs::filesystem::path &Path)
 {
     // crash the program if the file doesn't exist
-    VerifyElseCrash(std::filesystem::exists(Path));
+    VerifyElseCrash(std::__fs::filesystem::exists(Path));
 
     std::vector<std::string> AllLines;
     std::ifstream FileStream(Path);
     std::string CurrentLine;
 
-    while (std::getline(FileStream, CurrentLine)) { AllLines.push_back(CurrentLine); }
+    while (std::getline(FileStream, CurrentLine))
+    {
+        AllLines.push_back(CurrentLine);
+    }
 
     return AllLines;
 }
@@ -55,14 +58,20 @@ std::vector<std::bitset<bits>> Utilities::ConvertToBitset(const std::vector<std:
 {
     std::vector<std::bitset<bits>> Bitsets;
     Bitsets.reserve(Strings.size());
-    for (const std::string &StringBytes : Strings) { Bitsets.emplace_back(StringBytes); }
+    for (const std::string &StringBytes : Strings)
+    {
+        Bitsets.emplace_back(StringBytes);
+    }
     return Bitsets;
 }
 
 void Utilities::VerifyElseCrash(bool bAssert)
 {
     assert(bAssert);
-    if (!bAssert) { std::terminate(); }
+    if (!bAssert)
+    {
+        std::terminate();
+    }
 }
 
 void Utilities::VerifyNotReached()
@@ -74,12 +83,15 @@ std::vector<int> Utilities::ConvertToInt(const std::vector<std::string> &Vec)
 {
     std::vector<int> ReturnVec;
     ReturnVec.reserve(Vec.size());
-    for (const std::string &Str : Vec) { ReturnVec.push_back(std::stoi(Str)); }
+    for (const std::string &Str : Vec)
+    {
+        ReturnVec.push_back(std::stoi(Str));
+    }
 
     return ReturnVec;
 }
 
-std::vector<int> Utilities::GetBoardRow(const std::string& BasicString)
+std::vector<int> Utilities::GetBoardRow(const std::string &BasicString)
 {
     std::vector<int> ReturnVec;
     const int slots = 14;
@@ -87,14 +99,15 @@ std::vector<int> Utilities::GetBoardRow(const std::string& BasicString)
     for (int i = 1; i <= slots; i++)
     {
         std::string Number = BasicString.substr(i - 1, i + 1);
-        if (i % 3 == 1) ReturnVec.push_back(stoi(Number));
+        if (i % 3 == 1)
+            ReturnVec.push_back(stoi(Number));
     }
 
     return ReturnVec;
 }
-int Utilities::GetFirstLineLength(const std::filesystem::path &Path)
+int Utilities::GetFirstLineLength(const std::__fs::filesystem::path &Path)
 {
-    VerifyElseCrash(std::filesystem::exists(Path));
+    VerifyElseCrash(std::__fs::filesystem::exists(Path));
 
     std::vector<std::string> FirstLine;
     std::ifstream FileStream(Path);
