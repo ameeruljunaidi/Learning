c848c5d5ef8c94cc4f3d04268cbc2d257283790e
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-12-28 20:45:34 -0500

    Tried indexed set

---
+++ CPHandbook/IndexedSet.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+int main()
+{
+    cout << "test" << '\n';
+
+    return 0;
+}
